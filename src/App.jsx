21343e939f54542338131b12636cb70dd453d62b
Olena Surilova <elen21surilova@gmail.com>
2025-04-18 15:06:53 +0200

    feat: Gradient APIs

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,13 @@ function App() {
           3D Rotate
         </div>
       </div>
+      <div className='min-h-screen flex flex-col gap-12 items-center justify-center bg-gray-100'>
+        <div className='bg-linear-45 from-blue-500 to-green-500 p-8 rounded text-2xl font-bold text-white'>
+          Angled Gradient
+        </div>
+        <div className='size-64 rounded-full bg-conic-180 from-indigo-600 via-indigo-50 to-indigo-600'></div>
+        <div className='bg-radial from-yellow-500 to-red-500 h-64 w-64 rounded-full'></div>
+      </div>
     </>
     
   )
