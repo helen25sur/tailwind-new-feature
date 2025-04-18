5a59c3d93c4474e3aae5d4be9616b618677723f4
Olena Surilova <elen21surilova@gmail.com>
2025-04-18 13:48:25 +0200

    init: start project, @theme derective

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='bg-deepblue p-12'>
+      <h1 className='text-5xl font-bold text-alabaster'>
+        This is Tailwind CSS v4
+      </h1>
+      <button className='bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4'>Click</button>
+    </div>
+  )
+}
+
+export default App;
