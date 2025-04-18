d1beb2406c10b653b4adb175aa0fb29fd6f7a84e
Olena Surilova <elen21surilova@gmail.com>
2025-04-18 14:40:35 +0200

    feat: @plugin directive

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import './App.css';
 function App() {
 
   return (
-    <div className='bg-deepblue p-12'>
-      <div className="container">
-        <h1 className='text-5xl font-bold text-alabaster'>
-          This is Tailwind CSS v4
-        </h1>
-        <input type="text" placeholder='Enter your name' className='bg-white text-2xl text-amber border-4 p-4 rounded mr-4' />
-        <button className='bg-amber text-alabaster text-2xl font-bold mt-12 border-4 rounded px-6 py-4'>Click here</button>
+    <>
+      <div className='bg-deepblue p-12'>
+        <div className="container">
+          <h1 className='text-5xl font-bold text-alabaster'>
+            This is Tailwind CSS v4
+          </h1>
+          <input type="text" placeholder='Enter your name' className='bg-white text-2xl text-amber border-4 p-4 rounded mr-4' />
+          <button className='bg-amber text-alabaster text-2xl font-bold mt-12 border-4 rounded px-6 py-4 animate-pulse'>Click here</button>
+        </div>
       </div>
-
-    </div>
+      <div className='@container border p-4'>
+        <div className='grid grid-cols-1 @sm:grid-cols-2 @lg:grid-cols-4 gap-4'>
+          <div className='bg-blue-500 h-40'></div>
+          <div className='bg-green-500 h-40'></div>
+          <div className='bg-red-500 h-40'></div>
+          <div className='bg-yellow-500 h-40'></div>
+        </div>
+      </div>
+    </>
+    
   )
 }
 
