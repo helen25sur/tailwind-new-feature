1d2fe23b18159ca5e8d31547b17742b2c74115aa
Olena Surilova <elen21surilova@gmail.com>
2025-04-18 14:53:32 +0200

    feat: Transform APIs

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,11 @@ function App() {
           <div className='bg-yellow-500 h-40'></div>
         </div>
       </div>
+      <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+        <div className='bg-blue-500 h-96 w-96 flex items-center justify-center text-white text-4xl font-bold transform rotate-x-35 rotate-y-35'>
+          3D Rotate
+        </div>
+      </div>
     </>
     
   )
